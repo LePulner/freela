5b5ac4bae31e27873aa3edeefdb1f80f86422df9
Leticia Pulner <leticia.pulner@hotmail.com>
2019-05-03 15:10:12 -0300

    oi

---
+++ ReceitaApp - base/ReceitaApp/Trabalho.swift
@@ -0,0 +1,27 @@
+//
+//  Trabalho.swift
+//  ReceitaApp
+//
+//  Created by Leticia Linhares Camargo Pulner on 03/05/19.
+//  Copyright © 2019 APPLE DEVELOPER ACADEMY. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Trabalho {
+    var imgTrabalho:UIImage
+    var descricaoTrabalho:String
+    var numCurtidas:Int
+    
+    init(imgTrabalho:UIImage, descricaoTrabalho:String, numCurtidas:Int ) {
+        self.imgTrabalho = imgTrabalho
+        self.descricaoTrabalho = descricaoTrabalho
+        self.numCurtidas = numCurtidas
+        
+    }
+    
+    
+    
+}
--- ReceitaApp - base/ReceitaApp/ViewController.swift
+++ ReceitaApp - base/ReceitaApp/ViewController.swift
@@ -7,16 +7,43 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
-
+    
+    var indice = 0
+    @IBOutlet weak var imagemDestaque: UIImageView!
+    let trabalhos = [
+        Trabalho(imgTrabalho: UIImage(named: "leite")!, descricaoTrabalho: "Identidade Visual etc tal", numCurtidas: 15),
+        Trabalho(imgTrabalho: UIImage(named: "lettering")!, descricaoTrabalho: "Id de letteringzinho", numCurtidas: 5),
+        Trabalho(imgTrabalho: UIImage(named: "moça")!, descricaoTrabalho: "Pláaaaastico", numCurtidas: 3),
+        Trabalho(imgTrabalho: UIImage(named: "rótulo")!, descricaoTrabalho: "rótulinho", numCurtidas: 70),
+        
+    ]
+  
     override func viewDidLoad() {
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagemDestaque.image = trabalhos[indice].imgTrabalho
+    }
+    
+    @IBAction func entrarNoProjeto(_ sender: Any) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "projeto") as? ProjetoViewController{
+            self.navigationController?.show(vc,sender: self)
+        }
+    }
+    
+
+    @IBAction func Swiper(_ sender: Any) {
+        
+        if indice == 3 {
+            indice = 0
+        } else {
+            indice += 1
+        }
+        imagemDestaque.image = trabalhos[indice].imgTrabalho
     }
     
     
-
-
+    
 }
 
